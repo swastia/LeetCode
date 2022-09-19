b5a3ce3a1026c713af5b4410de05927a96732134
Swasti Arya <swasti.arya.eb@gmail.com>
2022-09-19 23:36:22 +0530

    Backspace String Compare

---
+++ src/com/learn/easy/BackspaceStringCompare_844.java
@@ -0,0 +1,108 @@
+package com.learn.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+
+
+Example 1:
+
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+*/
+public class BackspaceStringCompare_844 {
+
+    // Brute Force,  we can also use stack for pushing and popping characters
+    public static boolean backspaceCompare(String s, String t) {
+        List<Character> s1 = getResult(s), t1 = getResult(t);
+        if(s1.size() != t1.size()){
+            return false;
+        }
+
+        String rs1 = s1.stream().map(String::valueOf).collect(Collectors.joining());
+        String rt1 = t1.stream().map(String::valueOf).collect(Collectors.joining());
+
+        return rs1.equals(rt1);
+    }
+
+    private static List<Character> getResult(String str){
+        List<Character> result = new ArrayList<>();
+
+        for(int i=0; i<str.toCharArray().length; i++){
+            if(str.charAt(i) == '#'){
+                // when list is empty, do nothing
+                if(result.size() == 0){
+                    continue;
+                }else{
+                    result.remove(result.size()-1);
+                }
+            } else{
+                result.add(str.charAt(i));
+            }
+        }
+
+    return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(backspaceCompare("ab#c", "ad#c"));
+        System.out.println(backspaceCompare("a#b#c#", "a#d#c#"));
+        System.out.println(backspaceCompare("a###c", "a#d#c"));
+        System.out.println(backspaceCompare("a#b#c", "ad#c"));
+
+        System.out.println(backspaceCompareOptimized("ab#c", "ad#c"));
+        System.out.println(backspaceCompareOptimized("a#b#c#", "a#d#c#"));
+        System.out.println(backspaceCompareOptimized("a###c", "a#d#c"));
+        System.out.println(backspaceCompareOptimized("a#b#c", "ad#c"));
+
+    }
+
+    // Below is an optimal solution
+    //TODO: To fix
+
+    public static boolean backspaceCompareOptimized(String s, String t) {
+        int ps = s.charAt(s.toCharArray().length-1), pt = t.charAt(t.toCharArray().length -1);
+
+        while(ps >= 0 || pt >= 0){
+            int sCount=0, tCount=0;
+
+            //check if char at current index is #
+            if(s.charAt(ps) == '#'){
+                sCount +=1;
+                ps--;
+            } else {
+                // delete sCount number of chars
+                if(sCount > 0){
+                    ps--;
+                    sCount -= sCount;
+                }
+            }
+            if(t.charAt(pt) == '#'){
+                tCount += 1;
+                pt--;
+            } else{
+                if(tCount > 0){
+                    pt--;
+                    tCount -= tCount;
+                }
+            }
+
+            if(s.charAt(ps) != '#' && t.charAt(pt) != '#'){
+                if(s.charAt(ps) != s.charAt(pt)) {
+                    return false;
+                }
+                else{
+                    ps--;
+                    pt--;
+                }
+            }
+        }
+        return true;
+    }
+}
